e5944ad320c58b72156a8d7f3a512f0537bf0c42
Rob <miesvan88@gmail.com>
2021-04-23 02:18:32 -0500

    funner game

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ function getRandomArray() {
 let being = {};
 let pellet = {};
 let blockers = {};
+let water = {};
+let camSize = 16;
 let score = 0;
 let grid = {};
-let size = 16;
-let gridSize = 32;
+let size = 32;
+let gridSize = 128;
 var stop = false;
 var frameCount = 0;
-var fps = 10, fpsInterval, startTime, now, then, elapsed;
+var fps = 20, fpsInterval, startTime, now, then, elapsed;
 
 var canvas = document.querySelector("#viewport");
 var context = canvas.getContext("2d");
@@ -27,7 +29,6 @@ function check(e) {
 }
 
 function _initialize() {
-    console.log("1 - initializing...");
     stop = false;
     being.x = 0;
     being.y = 0;
@@ -35,16 +36,15 @@ function _initialize() {
     being.dy = 0;
     pellet.x = Math.floor((Math.random() * gridSize));
     pellet.y = Math.floor((Math.random() * gridSize));
-    console.log("Pellet located", pellet.x, pellet.y);
-    canvas.width = size * gridSize;
-    canvas.height = size * gridSize;
+    canvas.width = size * camSize * 2;
+    canvas.height = size * camSize * 2;
     context.clearRect(0,0,canvas.width, canvas.height);
     window.addEventListener('keydown',this._handleKey,false);
+    
     _startAnimating();
 }
 
 function _handleKey(e) {
-    //console.log("key pressed! ", e.keyCode);
     let k = e.keyCode;
     switch(k) {
         case 37:
@@ -71,21 +71,35 @@ function _handleKey(e) {
                 being.dy = 1;
             }
             break;
+        case 32:
+            fps = fps == 30 ? 10 : 30;
+            fpsInterval = 1000 / fps;
+            break;
     }
 }  
 
 function _logic() {
     being.x = being.x + being.dx;
     being.y = being.y + being.dy;
+
     let s_collision = grid[hashCode(being.x,being.y)] != undefined || being.x < 0 || being.x > gridSize || being.y < 0 || being.y > gridSize;
     let b_collision = blockers.hasOwnProperty(hashCode(being.x,being.y));
+    let underwater = water[hashCode(being.x,being.y)];
     let collision = s_collision || b_collision;
+
     if(collision) {
         stop = true;
         score = 0;
+        water = {};
         _clearGrid();
         _initialize(); 
-    } else {
+    } else if (underwater) {
+        fps = 10;
+        fpsInterval = 1000 / fps;
+    }
+    else {
+        fps = 20;
+        fpsInterval = 1000 / fps;
         grid[hashCode(being.x,being.y)] = 1;
     }
     let point = pellet.x == being.x && pellet.y == being.y;
@@ -95,7 +109,6 @@ function _logic() {
 }
 
 function _startAnimating() {
-    console.log("2 - beginning animation...")
     fpsInterval = 1000 / fps;
     then = Date.now();
     startTime = then;
@@ -111,31 +124,65 @@ function _animate() {
             then = now - (elapsed % fpsInterval);
             frameCount++;
             _logic();
-            _draw();
+            context.clearRect(0,0,canvas.width, canvas.height);
+            raster_draw();
         }
     }
 }
 
-function _draw() {
-    context.fillStyle = 'green';
-    context.fillRect(being.x*size,being.y*size,size,size);
-    context.strokeStyle = 'black';
-    context.rect(being.x*size,being.y*size,size,size);
-    context.stroke();
-    context.fillStyle = 'white';
-    context.fillRect(pellet.x*size,pellet.y*size,size,size);
-    context.fillStyle = 'red';
-    document.getElementById("score").innerHTML = score;
-    document.getElementById("pellet").innerHTML = "(" + pellet.x + ", " + pellet.y + ")";
-    document.getElementById("grid").innerHTML = gridSize + "x" + gridSize;
-    document.getElementById("frames").innerHTML = frameCount;
-    for(const o in blockers) {
-        
-        let rx = getX(`${o}`);
-        let ry = getY(`${o}`);
-        console.log(`${o}`, rx, ry);
-        context.fillRect(rx*size, ry*size,size,size);
+function raster_draw() {
+    let boundX0 = being.x - camSize;
+    let boundX1 = being.x + camSize;
+    let boundY0 = being.y - camSize;
+    let boundY1 = being.y + camSize;
+    
+    //Dynamic Objects
+    for(let i = boundX0; i < boundX1; i++) {
+        for(let j = boundY0; j < boundY1; j++) {
+            let b_found = blockers[hashCode(i,j)];
+            let s_found = grid[hashCode(i,j)];
+            let w_found = water[hashCode(i,j)];
+            if(w_found) {
+                context.fillStyle = 'blue';
+                context.fillRect((i - boundX0)*size,(j - boundY0)*size,size,size);
+            }
+            if(b_found) {
+                context.fillStyle = 'red';
+                context.fillRect((i - boundX0)*size,(j - boundY0)*size,size,size);
+            }
+            if(s_found) {
+                context.fillStyle = 'green';
+                context.fillRect((i - boundX0)*size,(j - boundY0)*size,size,size);
+            }
+        }
     }
+    context.stroke();
+
+    //Dynamic Walls
+    context.fillStyle = 'red';
+    if(boundX1 > gridSize) {
+        context.fillRect((gridSize - boundX0)*size,0,(boundX1 - gridSize)*size,gridSize*size);
+    } else if(boundX0 < 0) {
+        context.fillRect(0,0,-boundX0*size,gridSize*size);
+    }
+    if(boundY1 > gridSize) {
+        context.fillRect(0,(gridSize - boundY0)*size,gridSize*size,(boundY1-gridSize)*size); 
+    } else if(boundY0 < 0) {
+        context.fillRect(0,0,gridSize*size,-boundY0*size);
+    }
+
+    
+    //Dynamic Pellet
+    context.fillStyle = 'white';
+    let tx = pellet.x < boundX0 ? boundX0 : pellet.x;
+    tx = pellet.x > boundX1 ? boundX1 - 1 : tx;
+    let ty = pellet.y < boundY0 ? boundY0 : pellet.y;
+    ty = pellet.y > boundY1 ? boundY1 - 1 : ty;
+    context.fillRect((tx - boundX0)*size,(ty - boundY0)*size,size,size);
+
+    //Static Snake
+    context.fillStyle = 'green';
+    context.fillRect(camSize*size,camSize*size,size,size);
 }
 
 function hashCode(x, y) {
@@ -156,17 +203,20 @@ function getY(hash) {
 function _handlePellet() {
     pellet.x = Math.floor((Math.random() * (gridSize-2))) + 1;
     pellet.y = Math.floor((Math.random() * (gridSize-2))) + 1;
-    console.log("Pellet located", pellet.x, pellet.y);
     score = score + 1;
     _clearGrid();
+    _addBlocks();
+    _addWater();
+}
+
+function _addBlocks() {
     let rx, ry;
-    //console.log("Adding blockers...", score);
     for(let i = 0; i < score; i++) {
         rx = Math.floor((Math.random() * gridSize));
         ry = Math.floor((Math.random() * gridSize));
         if(rx != being.x && ry != being.y) {
             blockers[hashCode(rx, ry)] = 1;
-            for(let j = 0; j <= i / 5; j++) {
+            for(let j = 0; j <= i / 2; j++) {
                 if(Math.abs(being.x - rx) > j && Math.abs(being.y - ry) > j) {
                     blockers[hashCode(rx+j, ry)] = 1;
                     blockers[hashCode(rx-j, ry)] = 1;
@@ -180,6 +230,22 @@ function _handlePellet() {
             }
         }
     }
+}
+
+function _addWater() {
+    let rx, ry, s;
+    rx = Math.floor((Math.random() * gridSize));
+    ry = Math.floor((Math.random() * gridSize));
+    s = Math.floor((Math.random() * gridSize / 4));
+    for(let j = -s; j < s; j++) {
+        for(let k = -s; k < s; k++) {
+            let isCircle = Math.sqrt(((j*j) + (k*k))) < s;
+            if(isCircle) {
+                water[hashCode(rx+j,ry+k)] = 1;
+            }
+        }
+    }
+    
 }
 
 function _clearGrid() {
